53076338a4cfe0121849161239184235671196bb
Bluewy <bluewyechache@gmail.com>
2021-05-12 13:54:56 +0200

    add bot.py

---
+++ bot.py
@@ -0,0 +1,26 @@
+import discord
+import config
+import traceback
+
+from discord.ext import commands
+
+
+bot = commands.AutoShardedBot(
+    command_prefix=commands.when_mentioned_or('!'),
+    intents=discord.Intents.all(),
+    max_messages=5000,
+    status=discord.Status.online,
+    activity=discord.Activity(type=discord.ActivityType.playing, name=f'With my maker'),
+    description=f"I have no clear, main purpose just yet."
+)
+
+for extension in config.extensions:
+    try:
+        bot.load_extension(extension)
+        print(f'[EXTENSION] {extension} has loaded successfully.')
+    except Exception as e:
+        tb = traceback.format_exception(type(e), e, e.__traceback__)
+        tbe = "".join(tb) + ""
+        print(f'[WARNING] Could not load extension {extension}: {tbe}')
+
+bot.run(config.bottoken)
