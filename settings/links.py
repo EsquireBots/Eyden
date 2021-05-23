96512e091d78aab1007f33289ce6a3a5fc99e8d7
Bluewy <bluewyechache@gmail.com>
2021-05-23 12:15:33 +0200

    Add normal invite link

--- settings/links.py
+++ settings/links.py
@@ -5,3 +5,4 @@ Flitzrep = "https://discordrep.com/u/809057677716094997#"
 Flitzdiscord = "https://discord.com/users/809057677716094997"
 
 Invite_admin = "https://discord.com/oauth2/authorize?client_id=841670120178450472&permissions=8&scope=bot%20applications.commands"
+Invite_normal = "https://discord.com/api/oauth2/authorize?client_id=841670120178450472&permissions=67488833&scope=bot%20applications.commands"
