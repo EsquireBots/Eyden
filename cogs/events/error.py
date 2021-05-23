6ca6b26e3fa590c3b8a6a7f31d848fe5ec399d1f
Bluewy <bluewyechache@gmail.com>
2021-05-23 12:14:36 +0200

    More errors

--- cogs/events/error.py
+++ cogs/events/error.py
@@ -1,6 +1,3 @@
-import discord
-import traceback
-
 from discord.ext import commands
 from settings import emotes
 
@@ -8,7 +5,6 @@ from settings import emotes
 class error(commands.Cog, name="Errors"):
     def __init__(self, bot):
         self.bot = bot
-
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, err):
@@ -24,8 +20,22 @@ class error(commands.Cog, name="Errors"):
         if isinstance(err, commands.PrivateMessageOnly):
             return await ctx.send(f"{emotes.cross} **This command can only be used in Direct Messages.**")
 
+        if isinstance(err, commands.NoPrivateMessage):
+            return await ctx.send(f"{emotes.cross} **This command can only be used in guilds.**")
+
         if isinstance(err, commands.MissingRequiredArgument):
             return await ctx.send(f"{emotes.discovery} **You are missing required arguments - {err.param}**")
+
+        if isinstance(err, commands.MissingPermissions):
+            perms = "`" + '`, `'.join(err.missing_perms) + "`"
+            return await ctx.send(f"{emotes.cross} **You are missing permissions - {perms.replace('_', ' ')}**")
+
+        if isinstance(err, commands.BotMissingPermissions):
+            perms = "`" + '`, `'.join(err.missing_perms) + "`"
+            return await ctx.send(f"{emotes.cross} **I am missing permissions - {perms.replace('_', ' ')}**")
+
+        if isinstance(err, commands.NotOwner):
+            return await ctx.send(f"{emotes.cross} **Only bot owners can use this command.**")
 
 
 def setup(bot):
